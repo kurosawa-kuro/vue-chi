edc73fbd01578edaa8f957c883dad80dc52e0287
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-07-27 02:05:31 +0900

    init

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex h-[100dvh] overflow-hidden">
+    <!-- Sidebar -->
+    <Sidebar
+      :sidebarOpen="sidebarOpen"
+      @close-sidebar="sidebarOpen = false"
+      variant="v2"
+    />
+
+    <!-- Content area -->
+    <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <!-- Site header -->
+      <Header
+        :sidebarOpen="sidebarOpen"
+        @toggle-sidebar="sidebarOpen = !sidebarOpen"
+        variant="v2"
+      />
+
+      <!-- Main content -->
+      <main class="grow">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Sidebar from '@/partials/SimpleSidebar.vue'
+import Header from '@/partials/SimpleHeader.vue'
+
+const sidebarOpen = ref(false)
+</script> 
